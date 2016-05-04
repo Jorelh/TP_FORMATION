a0f16a7b532e16624faf861bb4402e98942c0a9b
Maxime Beugnon <noach.design@gmail.com>
2016-05-04 12:51:24 +0200

    Style
    Ajout du style

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-index.php
+<?php // index.php
+include 'inc/db.php';
+include 'inc/query.php';
+include 'inc/headerFront.php';
+
+
+$movies = randomMovies();
+
+foreach ($movies as $movie) {
+  echo '<div class="thumbnail"<img src="posters/'.$movie['id'].'.jpg" alt="#" /></div>';
+}
+
+?>
+
+
+<button type="button" name="button">Voir plus de flims</button>
+
+<?php
+include 'inc/footerFront.php';
+?>
