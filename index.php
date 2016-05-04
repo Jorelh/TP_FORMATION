6bb4b639003247dd002461b2c3db87b32ba9bf27
Johann <johannhild@gmail.com>
2016-05-04 15:08:42 +0200

    Index.php => Correction erreur si image n'existe pas.

--- index.php
+++ index.php
@@ -14,8 +14,9 @@ $movies = randomMovies();
 foreach ($movies as $movie) {
   echo '<a href="details.php?id='.$movie['id'].'&title='.$movie['slug'].'" alt="#">';
   echo '<div class="thumbnail">';
-
-  if(file_exists($movie['id'].'.jpg')){
+  $filename = 'posters/' . $movie['id'] . '.jpg';
+  // test si $filename existe et si elle n'existe pas, la remplace par une image par défaut.
+  if(file_exists($filename)){
     echo '<img height="310" width="220" src="posters/'.$movie['id'].'.jpg" alt="#" />';
   }else{
     echo '<img height="310" width="220" src="posters/no-image.png" alt="#" />';
