b80b4fd6ca3242caf96301e51e49cf5e03c88674
Jo'h <jorelh@live.fr>
2016-05-04 12:42:22 +0200

    fonction =>slug url
    // - Pretty Url
    // - L’url de ces pages de détails devrait contenir la version
    normalisée du titre du film (slug). Et tant qu’à y être, faites tout
    votre possible pour référencer au mieux cette page par rapport au titre
    du film et de son année de réalisation.

--- inc/fonctions.php
+++ inc/fonctions.php
@@ -6,3 +6,35 @@ function debug($tab){
   print_r($tab);
   echo '</pre>';
 }
+
+//slug
+function slugify($text){
+
+  // remplace les caractères non alphanumérique par _
+  $text = preg_replace('~[^\pL\d]+~u', '_', $text);
+
+  // transliterate
+  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+  // supprime les caractères indésirables 
+  $text = preg_replace('~[^-\w]+~', '', $text);
+
+  // trim
+  $text = trim($text, '-');
+
+  // remove duplicate -
+  $text = preg_replace('~-+~', '-', $text);
+
+  // lowercase
+  $text = strtolower($text);
+
+  if (empty($text)){
+
+    return 'n-a';
+
+  }else{
+
+  return $text;
+  }
+}
+
