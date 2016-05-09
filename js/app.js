9318f7d3677820136952c64f2f012fd10ff8b60f
Maxime Beugnon <noach.design@gmail.com>
2016-05-09 16:56:15 +0200

    fin de journée
    Lien jQuery corrigé

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
 // Recherche dropdown
 
-$(document).ready(function()){
+
+
+$(document).ready(function(){
   console.log ("bouton ta mère");
    //alert('bouton ta mère');
-	$(".buttonsearch").click(function() {
+$(".buttonsearch").click(function() {
 
-	  if ($(".dropdown").hasClass(".hide")){
+  if ($(".dropdown").hasClass(".hide")){
 
-	  $(".dropdown").removeClass(".hide");
+  $(".dropdown").removeClass(".hide");
 
-	  } else {
-	    $(".dropdown").addClass(".hide");
-	  }
-	});
-}
+  } else {
+    $(".dropdown").addClass(".hide");
+  }
+});
+
+});
